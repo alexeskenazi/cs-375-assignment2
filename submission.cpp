cee69bf57e303f6c74c359bc2eab14bc2dcd8b60
Alex Eskenazi <aeskena1@binghamton.edu>
2025-09-24 10:05:37 -0400

    First pass at brute force calcualtion

--- submission.cpp
+++ submission.cpp
@@ -3,154 +3,65 @@
 #include <cstring>
 #include <cassert>
 #include <cmath>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
 #define BUFSIZE 1000
 
+struct Point {
+    long long x;
+    long long y;
+};
 
-long long findNumLines(FILE *fp);
-
-
+double bruteForceClosest(const vector<Point>& pts, int& bestI, int& bestJ);
 
 FILE *srcFP, *destFP;
 long long numLines, *data;
 
 int main(int argc, char *argv[]) {
-
-  int i;
-  char str[BUFSIZE];
-
-  // check command line args
-  if(argc<3) {
-    printf("Usage: progname <input_file> <output_file>");
-    exit(0);
-  }
-
-  // run tests if "test" argument provided
-  if(argc>3) {
-    if(strcmp(argv[3], "test") == 0) {
-      // testSortABCarray();
-      // testFindKeyAsDifference();
-      exit(0);
-    }
-  }
-  
-  // open input and output files
-  if((srcFP =fopen(argv[1], "r")) == NULL) {
-    perror("Error opening input file");
-    exit(0);
-  }
-
-  if((destFP = fopen(argv[2], "w")) == NULL) {
-    perror("Error opening output file");
-    exit(0);
-  }
-  
-  // count lines in file
-  numLines = findNumLines(srcFP);
-  
-
-  // find first real line (skip blanks and comments)
-  for(i=0; i<numLines; i++) {
-    fgets(str, BUFSIZE, srcFP);
-
-    // skip blank lines and comments
-    if((str[0] == '\n') || (str[0] == ' ') || ((str[0] == '/') && (str[1] == '/'))) {
-      i--;
-      continue;
+    int n = 10000;
+    vector<Point> p(n);
+    for (int i = 0; i < n; ++i) {
+            p[i].x = n - i;
+            p[i].y = n - i;
     }
 
-    break;
-  }
+    int iBest, jBest;
+    clock_t t0 = clock();
+    double bestDist = bruteForceClosest(p, iBest, jBest);
+    clock_t t1 = clock();
 
+    double ms = 1000.0 * (t1 - t0) / CLOCKS_PER_SEC;
 
-  // Part B.1 - sort ABC strings
-  fprintf(destFP, "//Part B.1 \n\n");
-  printf("//Part B.1 \n\n");
-  
-  while(!feof(srcFP)) {
-
-    // process the line we already read
-    int len = strlen(str);
-    if (len > 0) { 
-      // sortABCarray(destFP, str, len);
-    }
-    
-    fgets(str, BUFSIZE, srcFP);
-
-    // skip blank lines
-    if(( str[0] == '\n') || (str[0] == ' ') ) {
-      str[0] = '\0';
-      continue;
-    }
-
-    // stop when we hit Part B.2
-    if(strncmp(str, "//Part B.2", 10) == 0) {
-      break;
-    }
-
-  }
-
-  // Part B.2 - read numbers into array
-  data = (long long *) malloc(numLines * sizeof(long long));
-  int dataIndex = 0;
-
-  // read numbers from file
-  for(i=0; i<numLines; i++) {
-
-    for(int j=0; j<100; j++) {
-      str[j] = '\0';
-    }
-
-    fgets(str, 100, srcFP);
-
-    // skip blank lines and comments
-    if((str[0] == '\n') || (str[0] == ' ') || ((str[0] == '/') && (str[1] == '/'))) {
-      i--;
-      continue;
-    }
-
-    // stop at end of file
-    if(feof(srcFP)) {
-      break;
-    }
-
-    // convert string to number and store
-    sscanf(str, "%lld", &(data[dataIndex]));
-    dataIndex++;
-  }
-
-  fprintf(destFP, "\n\n// Part B.2\n\n");
-  printf("\n\n// Part B.2\n\n");
-
-  // findKeyAsDifference(destFP, data, dataIndex);
-
-  fclose(srcFP);
-  fclose(destFP);
-}
-
-
-// count how many lines are in the file
-long long findNumLines(FILE *fp) {
-
-  long long numLines = 0;
-  char str[100];
-
-  // count each line
-  while(!feof(fp)) {
-    fgets(str, 100, fp);
-    numLines++;
-  }
-  
-  numLines--; // fix off-by-one from EOF
-  
-  // go back to start of file
-  if(fseek(fp, 0L, SEEK_SET) == EOF) {
-    perror("Error while seeking to begining of file");
-    exit(0);
-  }
+    cout << "Brute-force closest pair:\n";
+    cout << "  i = " << iBest << "  (" << p[iBest].x << ", " << p[iBest].y << ")\n";
+    cout << "  j = " << jBest << "  (" << p[jBest].x << ", " << p[jBest].y << ")\n";
+    cout << "  distance = " << bestDist << "\n";
+    cout << "Time (ms): " << ms << "\n";
+    return 0;
 
   return numLines;
 }   
 
+
+// go through all pairs of points and find the closest pair
+double bruteForceClosest(const vector<Point>& pts, int& bestI, int& bestJ) {
+    long double bestDist = 1e18; // novice-friendly large value
+    bestI = -1; bestJ = -1;
+    int n = (int)pts.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            long long dx = pts[i].x - pts[j].x;
+            long long dy = pts[i].y - pts[j].y;
+            long double dist = sqrt((long double)dx * dx + (long double)dy * dy);
+            if (dist < bestDist) {
+                bestDist = dist;
+                bestI = i;
+                bestJ = j;
+            }
+        }
+    }
+    return bestDist;
+}

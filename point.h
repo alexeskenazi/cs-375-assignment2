6d32e8130bcf218e642278731c0309b18d7cf43c
Alex Eskenazi <aeskena1@binghamton.edu>
2025-09-25 07:32:01 -0400

    Moving Point def and functions to a new file

---
+++ point.h
@@ -0,0 +1,21 @@
+#pragma once
+
+struct Point {
+    long long x;
+    long long y;
+    long long z;
+};
+
+bool compareX(const Point& a, const Point& b) {
+    return a.x < b.x;
+}
+
+bool compareY(const Point& a, const Point& b) {
+    return a.y < b.y;
+}
+
+long double dist(const Point& a, const Point& b) {
+    long long dx = a.x - b.x;
+    long long dy = a.y - b.y;
+    return sqrt((long double)dx * dx + (long double)dy * dy);
+}

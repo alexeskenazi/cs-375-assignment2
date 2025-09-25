0f3ef6882633286e696c602485d29c6b33e42049
Alex Eskenazi <aeskena1@binghamton.edu>
2025-09-25 18:52:34 -0400

    Missed in last commit

--- submission.cpp
+++ submission.cpp
@@ -1,26 +1,105 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
 #include <cassert>
 #include <cmath>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include "point.h"
+#include "read_input_file.h"
 
 using namespace std;
 
-double bruteForceClosest(const vector<Point>& pts, int& bestI, int& bestJ);
 
-FILE *srcFP, *destFP;
-long long numLines, *data;
+double bruteForceClosest(const vector<Point>& pts, int& bestI, int& bestJ) {
+    long double bestDist = 1e18;  // largest value possible
+    bestI = -1; bestJ = -1;
+    int n = (int)pts.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            long double d = dist(pts[i], pts[j]);
+            if (d < bestDist) {
+                bestDist = d;
+                bestI = i;
+                bestJ = j;
+            }
+        }
+    }
+    return bestDist;
+}
+
+long double closestPairRec(vector<Point>& Px, vector<Point>& Py, int& bestI, int& bestJ) {
+    int n = Px.size();
+    int mid = n / 2;
+    Point midPoint = Px[mid];
+
+    vector<Point> Qx(Px.begin(), Px.begin() + mid);
+    vector<Point> Rx(Px.begin() + mid, Px.end());
+
+    vector<Point> Qy, Ry;
+    for (const auto& p : Py) {
+        if (p.x <= midPoint.x)
+            Qy.push_back(p);
+        else
+            Ry.push_back(p);
+    }
+
+    int bestIL = -1, bestJL = -1, bestIR = -1, bestJR = -1;
+    long double dl = (Qx.size() > 1) ? closestPairRec(Qx, Qy, bestIL, bestJL) : 1e18;
+    long double dr = (Rx.size() > 1) ? closestPairRec(Rx, Ry, bestIR, bestJR) : 1e18;
+
+    long double d = dl;
+    int offset = 0;
+    if (dr < dl) {
+        d = dr;
+        offset = mid;
+        bestI = bestIR + offset;
+        bestJ = bestJR + offset;
+    } else {
+        bestI = bestIL;
+        bestJ = bestJL;
+    }
+
+    // Build strip
+    vector<Point> strip;
+    for (const auto& p : Py) {
+        if (abs(p.x - midPoint.x) < d)
+            strip.push_back(p);
+    }
+
+    // Check strip for closer pairs
+    for (int i = 0; i < (int)strip.size(); ++i) {
+        for (int j = i + 1; j < (int)strip.size() && (strip[j].y - strip[i].y) < d; ++j) {
+            long double d_strip = dist(strip[i], strip[j]);
+            if (d_strip < d) {
+                d = d_strip;
+                // Find indices in Px
+                for (int idx1 = 0; idx1 < n; ++idx1) {
+                    if (Px[idx1].x == strip[i].x && Px[idx1].y == strip[i].y) bestI = idx1;
+                    if (Px[idx1].x == strip[j].x && Px[idx1].y == strip[j].y) bestJ = idx1;
+                }
+            }
+        }
+    }
+    return d;
+}
+
+double divideAndConquerClosest(const vector<Point>& pts, int& bestI, int& bestJ) {
+    vector<Point> Px = pts;
+    vector<Point> Py = pts;
+    std::sort(Px.begin(), Px.end(), compareX);
+    std::sort(Py.begin(), Py.end(), compareY);
+    return closestPairRec(Px, Py, bestI, bestJ);
+}
+
 
 int main(int argc, char *argv[]) {
     int n = 10000;
     vector<Point> p(n);
     for (int i = 0; i < n; ++i) {
-            p[i].x = n - i;
-            p[i].y = n - i;
+        p[i].x = n - i;
+        p[i].y = n - i;
     }
 
     int iBest, jBest;
@@ -35,26 +114,17 @@ int main(int argc, char *argv[]) {
     cout << "  j = " << jBest << "  (" << p[jBest].x << ", " << p[jBest].y << ")\n";
     cout << "  distance = " << bestDist << "\n";
     cout << "Time (ms): " << ms << "\n";
+
+    int iBestDC, jBestDC;
+    clock_t t0dc = clock();
+    double bestDistDC = divideAndConquerClosest(p, iBestDC, jBestDC);
+    clock_t t1dc = clock();
+    double msDC = 1000.0 * (t1dc - t0dc) / CLOCKS_PER_SEC;
+
+    cout << "\nDivide and conquer closest pair:\n";
+    cout << "  i = " << iBestDC << "  (" << p[iBestDC].x << ", " << p[iBestDC].y << ")\n";
+    cout << "  j = " << jBestDC << "  (" << p[jBestDC].x << ", " << p[jBestDC].y << ")\n";
+    cout << "  distance = " << bestDistDC << "\n";
+    cout << "Time (ms): " << msDC << "\n";
     return 0;
-
-  return numLines;
-}   
-
-
-// go through all pairs of points and find the closest pair
-double bruteForceClosest(const vector<Point>& pts, int& bestI, int& bestJ) {
-    long double bestDist = 1e18; // novice-friendly large value
-    bestI = -1; bestJ = -1;
-    int n = (int)pts.size();
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            long double d = dist(pts[i], pts[j]);
-            if (d < bestDist) {
-                bestDist = d;
-                bestI = i;
-                bestJ = j;
-            }
-        }
-    }
-    return bestDist;
 }

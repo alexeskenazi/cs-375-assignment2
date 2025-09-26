e1f66de96af75cfa0646d562cfa8d827aff1bd6a
Alex Eskenazi <aeskena1@binghamton.edu>
2025-09-26 08:30:36 -0400

    Moved the point functions to a cpp file to avoid duplicate code errors

---
+++ point.cpp
@@ -0,0 +1,17 @@
+#include "point.h"
+#include <cmath>
+
+bool compareX(const Point& a, const Point& b) {
+    return a.x < b.x;
+}
+
+bool compareY(const Point& a, const Point& b) {
+    return a.y < b.y;
+}
+
+long double dist(const Point& a, const Point& b) {
+    long long dx = a.x - b.x;
+    long long dy = a.y - b.y;
+    long long dz = a.z - b.z;
+    return sqrt((long double)dx * dx + (long double)dy * dy + (long double)dz * dz);
+}
--- point.h
+++ point.h
@@ -6,16 +6,6 @@ struct Point {
     long long z;
 };
 
-inline bool compareX(const Point& a, const Point& b) {
-    return a.x < b.x;
-}
-
-inline bool compareY(const Point& a, const Point& b) {
-    return a.y < b.y;
-}
-
-inline long double dist(const Point& a, const Point& b) {
-    long long dx = a.x - b.x;
-    long long dy = a.y - b.y;
-    return sqrt((long double)dx * dx + (long double)dy * dy);
-}
+bool compareX(const Point& a, const Point& b);
+bool compareY(const Point& a, const Point& b);
+long double dist(const Point& a, const Point& b);
